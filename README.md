9c7312214e33b6c0e83e184e53f0912bac916cd9
Boston343 <branden.austin1@gmail.com>
2022-10-24 11:49:10 -0400

    most things already done. Figured I'd upload this in case I'm not at my main PC and want a reference.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # IntroTo-Mongoose
-Mongoose learning for working with MongoDB
+
+Mongoose learning for working with MongoDB.
+
+## Dependencies
+
+-   Node modules - inside project run `npm install`
+    -   mongoose
+    -   eslint - if desired for linting
+        -   [ESLint Getting Started Guide](https://eslint.org/docs/latest/user-guide/getting-started)
+
+## Includes
+
+-   JS includes
+    -   mongoose
+-   Mongoose
+    -   CRUD operations
+	-	Data validation
+	-	Async and sync functions
---
+++ app.js
@@ -0,0 +1,116 @@
+import mongoose from "mongoose";
+
+// -----------------------------------------------------------------------------------
+// Fruit example
+// this will connect to the db "fruitsDB", and will create it if it doesn't already exist
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+    useNewUrlParser: true,
+});
+
+const fruitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please check your data entry, no name specified!"],
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10,
+    },
+    review: String,
+});
+
+// mongoose will auto make it plural "fruits"
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const apple = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Pretty solid fruit.",
+});
+
+// save your new fruit into the database
+apple.save();
+
+const banana = new Fruit({
+    name: "banana",
+    rating: 10,
+    review: "Best.",
+});
+
+const kiwi = new Fruit({
+    name: "kiwi",
+    rating: 6,
+    review: "Alright, a lot of effort for little gain.",
+});
+
+// -----------------------------------------------------------------------------------
+// insert many example
+// Fruit.insertMany([banana, kiwi], (err, fruits) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(fruits);
+//     }
+// });
+
+// -----------------------------------------------------------------------------------
+// Person example
+
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+});
+
+// mongoose will auto make it plural "fruits"
+const Person = mongoose.model("Person", peopleSchema);
+
+const person = new Person({
+    name: "John",
+    age: 37,
+});
+
+// save your new person into the database
+// person.save();
+
+// -----------------------------------------------------------------------------------
+// deleteOne example. This is synchronous and uses "await" rather than a callback function
+const deletedFruit = await Fruit.deleteOne({ name: "Apple" });
+if (deletedFruit.deletedCount === 1) {
+    console.log("Apple was deleted.");
+} else {
+    console.log("There was an error and Apple was not deleted.");
+}
+
+// -----------------------------------------------------------------------------------
+// deleteMany. This is synchronous and uses "await" rather than a callback function
+const deletedPeople = await Person.deleteMany({ name: "John" });
+if (deletedPeople.deletedCount >= 1) {
+    console.log(deletedPeople.deletedCount + " Johns were deleted.");
+} else {
+    console.log("There was an error and Johns were not deleted.");
+}
+
+// -----------------------------------------------------------------------------------
+// Find example. "fruits" is an array of objects
+Fruit.find((err, fruits) => {
+    if (err) {
+        console.log(err);
+    } else {
+        mongoose.connection.close();
+
+        fruits.forEach((fruit) => {
+            console.log(fruit.name);
+        });
+    }
+});
+
+// -----------------------------------------------------------------------------------
+// Update example. These are async and for some reason this needs to be after find function...
+Fruit.updateOne({ name: "banana" }, { rating: 8 }, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully updated the document");
+    }
+});
